a5448708b77feeb37f20aeeeb2209b6c1984e3b6
kamipatel <kamipatel@gmail.com>
2019-06-25 18:20:21 -0500

    first commit

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sfdx force:org:create -f config/dhsetup-scratch-def.json -a dh_scratch -d 7
+sfdx force:org:create -f config/dhsetup-scratch-def.json -a ADK --setdefaultusername -d 7
 
-sfdx force:org:open -u dh_scratch
+sfdx force:org:open
