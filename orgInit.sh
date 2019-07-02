73fddb22384761c96105879adf683d891327c89e
kamipatel <kamipatel@gmail.com>
2019-07-02 18:29:28 -0500

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sfdx force:org:create -f config/dhsetup-scratch-def.json -a ADK --setdefaultusername -d 7
-
+sfdx force:apex:execute -f config/create-demo-data-setup.apex
 sfdx force:org:open
